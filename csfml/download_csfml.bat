03eab1070aef38c48687ba0ea1fe2677d6447c1a
causerp <causerp@users.noreply.github.com>
2025-02-08 18:12:04 +0000

    fix

--- csfml/download_csfml.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-git clone --branch 2.6.1 https://github.com/SFML/CSFML.git ./csfml
-move /Y csfml\* .
-rmdir /S /Q csfml
--- csfml/download_csfml.sh
+++
@@ -1,3 +0,0 @@
-git clone --branch 2.6.1 https://github.com/SFML/CSFML.git ./csfml
-mv -f ./csfml/* .
-rm -rf ./csfml/
